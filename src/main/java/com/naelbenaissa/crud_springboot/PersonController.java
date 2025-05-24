d3335d1c4e7910fb1c95a3a461ebda6caa190807
naelb <naelbenaissapro@gmail.com>
2025-05-24 00:40:21 -0400

    Get all and id and post fait

--- src/main/java/com/naelbenaissa/crud_springboot/PersonController.java
+++ src/main/java/com/naelbenaissa/crud_springboot/PersonController.java
@@ -1,8 +1,11 @@
 package com.naelbenaissa.crud_springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/persons")
@@ -12,5 +15,21 @@ public class PersonController {
         this.personRepository = personRepository;
     }
 
+    @GetMapping
+    public ResponseEntity<List<Person>> getAllPersons() {
+        return new ResponseEntity<>(personRepository.findAll(), HttpStatus.OK);
+    }
 
+    @PostMapping
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+        Person personCreated = personRepository.save(person);
+        return new ResponseEntity<>(personCreated, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPersonById(@PathVariable Long id) {
+        return personRepository.findById(id)
+                .map(person -> new ResponseEntity<>(person, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
